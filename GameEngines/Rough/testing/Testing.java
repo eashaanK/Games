0bda09932ead91a02f22acfdd981cf33fed42bef
eashaan <eashaan@ashishs-imac.home>
2014-10-14 17:15:41 -0400

    asasasa

--- GameEngines/Rough/testing/Testing.java
+++ GameEngines/Rough/testing/Testing.java
@@ -5,23 +5,41 @@ import processing.core.PApplet;
 public class Testing extends PApplet
 {
 
+	float x, y, z, a = PI / 3;
 	public void setup(){
 		size(900, 900, OPENGL);
 	}
 	
 	public void draw(){
-		background(255, 255, 255);
+		background(0);
+		this.lights();
+		
+		a += 0.1;
+		
+		beginCamera();
+		perspective();
+		rotateY((float) Math.toRadians(1));
+		endCamera();
+
+		
 		pushMatrix();
-		lights();
-		//translate(mouseX, mouseY, 0);
-		beginShape(POINTS);
-		fill(255, 0, 0);
-		vertex(30, 20);
-		vertex(85, 20);
-		vertex(85, 75);
-		vertex(30, 75);
-		endShape();
+		translate(50, 50, 0);
+		rotateY(a);
+		box(45);
 		popMatrix();
+		
+		printCamera();
+
+		
+	}
+	
+	public void keyPressed(){
+		if(keyCode == UP || keyCode == 'w' || keyCode == 'W')
+		{
+			System.out.println("Now moving forward");
+			z += 10;
+		}
+
 	}
 
 }
