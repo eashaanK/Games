3658f196fa9197b6160f16a29ac732420b0cabce
101040024 <101040024@HSS-101-22.westwindsor.k12.nj.us>
2014-10-14 14:50:07 -0400

    sdsf

--- GameEngines/Rough/testing/Testing.java
+++ GameEngines/Rough/testing/Testing.java
@@ -6,11 +6,22 @@ public class Testing extends PApplet
 {
 
 	public void setup(){
-		size(1000, 1000);
+		size(900, 900, OPENGL);
 	}
 	
 	public void draw(){
-		
+		background(255, 255, 255);
+		pushMatrix();
+		lights();
+		//translate(mouseX, mouseY, 0);
+		beginShape(POINTS);
+		fill(255, 0, 0);
+		vertex(30, 20);
+		vertex(85, 20);
+		vertex(85, 75);
+		vertex(30, 75);
+		endShape();
+		popMatrix();
 	}
 
 }
