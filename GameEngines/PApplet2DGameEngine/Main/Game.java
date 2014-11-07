bab63e7100ede1ad61d4cebc341c3975932db3b6
eashaanK <eashaan@kumar.co>
2014-11-06 21:23:33 -0500

    lolz

---
+++ GameEngines/PApplet2DGameEngine/Main/Game.java
@@ -0,0 +1,20 @@
+package Main;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void setup(){
+		size(800, 800);
+	}
+	
+	public void draw(){
+		
+	}
+
+}
---
+++ GameEngines/PApplet2DGameEngine/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import processing.core.PApplet;
+
+public class Main {
+
+	public static void main(String args[]) {
+	//	PApplet.main(new String[] {/* "--present", */"Main/Game" });
+	}
+}
