92affd1826a918942da8c355ec54e3e53a75c798
eashaan <eashaan@ashishs-imac.home>
2014-12-25 13:43:56 -0500

    dafdasf

--- Artificial Intellignce/Speech/speechRecognizer/HelloWorld.java
+++ Artificial Intellignce/Speech/speechRecognizer/HelloWorld.java
@@ -14,16 +14,17 @@
 
 package speechRecognizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import edu.cmu.sphinx.frontend.util.Microphone;
-import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.linguist.Linguist;
+import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.util.props.PropertyException;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 
 
 /**
@@ -31,32 +32,31 @@ import java.net.URL;
  * built using Sphinx-4. This application uses the Sphinx-4 endpointer,
  * which automatically segments incoming audio into utterances and silences.
  */
-public class TesterMain {
+public class HelloWorld {
     static ConfigurationManager cm;
-    /**
-     * Main method for running the HelloWorld demo.
-     */
+  
     public static void main(String[] args) {
         try {
             URL url;
             if (args.length > 0) {
                 url = new File(args[0]).toURI().toURL();
             } else {
-                url = TesterMain.class.getResource("helloworld.config.xml");
+                url = HelloWorld.class.getResource("helloworld.config.xml");
             }
 
-            System.out.println("Loading...");
+            System.out.println("Loading..." + url);
 
             cm = new ConfigurationManager(url);
+           
 
 	    Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
 	    Microphone microphone = (Microphone) cm.lookup("microphone");
 
 
-            /* allocate the resource necessary for the recognizer */
+     //        allocate the resource necessary for the recognizer */
             recognizer.allocate();
 
-            /* the microphone will keep recording until the program exits */
+         //    the microphone will keep recording until the program exits */
 	    if (microphone.startRecording()) {
 
 		System.out.println
@@ -124,4 +124,45 @@ public class TesterMain {
 
 	    }
 	}
+
+/*public static void main(String[] args) {
+    ConfigurationManager cm;
+
+    if (args.length > 0) {
+        cm = new ConfigurationManager(args[0]);
+    } else {
+    	File file = new File("Artificial Intellignce/Speech/helloworld.config.xml");
+    	System.out.println(file.getAbsolutePath());
+new ConfigurationManager("", 1);
+    	cm = new ConfigurationManager(HelloWorld.class.getResource(file.getAbsolutePath()));
+    }
+
+        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        // start the microphone or exit if the program if this is not possible
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        if (!microphone.startRecording()) {
+            System.out.println("Cannot start microphone.");
+            recognizer.deallocate();
+            System.exit(1);
+        }
+
+        System.out.println("Say: (Good morning | Hello) ( Bhiksha | Evandro | Paul | Philip     | Rita | Will )");
+
+        // loop the recognition until the programm exits.
+        while (true) {
+            System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+            edu.cmu.sphinx.result.Result result = recognizer.recognize();
+
+            if (result != null) {
+                String resultText = result.getBestFinalResultNoFiller();
+                System.out.println("You said: " + resultText + '\n');
+            } else {
+                System.out.println("I can't hear what you said.\n");
+            }
+        }
+    }   */ 
+
 
