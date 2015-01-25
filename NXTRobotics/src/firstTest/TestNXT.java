c21b58567e067f920794d6d46d69edc3367f546e
eashaan <eashaan@ashishs-imac.home>
2015-01-25 16:27:22 -0500

    YESS!!!! NXT ROBO WORKS!!!

---
+++ NXTRobotics/src/firstTest/TestNXT.java
@@ -0,0 +1,13 @@
+package firstTest;
+
+import lejos.nxt.Motor;
+
+
+public class TestNXT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Motor.A.rotate(100);
+	}
+
+}
