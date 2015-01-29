f535a9862a675a237d169bb03fdebdd86398dcb4
eashaan <eashaan@ashishs-imac.home>
2015-01-29 00:13:27 -0500

    LOL whuit

--- Testing/src/multilayer_shit/C.java
+++
@@ -1,9 +0,0 @@
-package multilayer_shit;
-
-public class C {
-
-	public static final String REGEX = "/";
-	public static final String FETCH_MESSAGE = "FETCH_MESSAGE";
-	public static final String FETCH_USERS = "FETCH_USERS";
-
-}
