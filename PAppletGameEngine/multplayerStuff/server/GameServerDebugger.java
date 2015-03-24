da4e63bd5db320105d56588878acb0b9c9335f6e
eashaanK <eashaan@kumar.co>
2015-03-24 10:19:30 -0400

    sdasdsf

---
+++ PAppletGameEngine/multplayerStuff/server/GameServerDebugger.java
@@ -0,0 +1,10 @@
+package server;
+
+import javax.swing.JFrame;
+
+public class GameServerDebugger extends JFrame{
+
+	public GameServerDebugger(String name, int x, int y){
+		super(name, x, y);
+	}
+}
